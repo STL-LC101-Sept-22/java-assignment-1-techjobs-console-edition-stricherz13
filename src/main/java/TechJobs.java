e17e28f615a3bb76aa27447a64112877fc864c31
stricherz13 <94708588+stricherz13@users.noreply.github.com>
2023-01-21 16:56:18 -0600

    printJobs method complete.

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,3 +1,4 @@
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +11,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +113,19 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println("\n" + "*****");
+            for (Map.Entry<String, String> j : job.entrySet()) {
+                System.out.println(j.getKey() + ": " + j.getValue());
+            }
+            System.out.println("*****");
+        }
     }
 }
